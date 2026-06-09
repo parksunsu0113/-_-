dbeeb807c80dd076788c5c96a5de389dd8129f8a
parksunsu0113 <parksunsu0113@gmail.com>
2026-06-10 08:08:39 +0900

    [level 0] Title: 수 조작하기 1, Time: 2.98 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int n, String control) {
+        int answer = n;
+        for(int i = 0; i < control.length(); i++){
+            char a = control.charAt(i);
+            
+            if(a == 'w') {
+                answer += 1;
+            } else if(a=='s'){
+                answer -= 1;
+            } else if (a == 'd'){
+                answer += 10;
+            } else if (a == 'a'){
+                answer -= 10;
+            }
+        }
+        
+        return answer;
+    }
+}
