41b75fb4ef47646d39c05f68c5635e58ac08986d
박경은 <parksunsu0113@gmail.com>
2026-07-13 22:05:40 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.04 ms, Memory: 81.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] solution(int[] nu, int n) {
+        int[] answer = new int[n];
+        for(int i = 0; i < n; i++) {
+            answer[i] = nu[i];
+        }
+        return answer;
+    }
+}
